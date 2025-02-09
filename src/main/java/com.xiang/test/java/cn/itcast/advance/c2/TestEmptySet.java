6e7a7a2319baefd5ff47f60268ac56e59897e931
xiaoguo-19years <140319155+xiaoguo-19years@users.noreply.github.com>
2025-02-09 17:33:08 +0800

    package rename

--- src/main/java/com.xiang/test/java/cn/itcast/advance/c2/TestEmptySet.java
+++
@@ -1,9 +0,0 @@
-package com.xiang.advance.c2;
-
-import java.util.Collections;
-
-public class TestEmptySet {
-    public static void main(String[] args) {
-        Collections.EMPTY_SET.add("o");
-    }
-}
